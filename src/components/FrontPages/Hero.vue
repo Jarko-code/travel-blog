58d25f7ba0d4e7a1d9295a2159229d68114ed682
Filip Vacko <filip.vacko@gmail.com>
2025-03-21 16:27:54 +0100

    bring back uncommented code

--- src/components/FrontPages/Hero.vue
+++ src/components/FrontPages/Hero.vue
@@ -4,14 +4,13 @@
       class="bg-[url('../assets/images/bg.jpg')] h-screen bg-cover bg-black/30 bg-blend-overlay text-white flex items-start"
     >
       <Navigation />
-       <!-- <div
+       <div
         id="header-subscription"
         v-if="route.name === '/'"
        >
         <slot name="header"></slot>
         <slot name="subscription"></slot>
-       </div> -->
-       <slot name="subscription"></slot>
+       </div>
     </div>
 </template>
 
