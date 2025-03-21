09f22891fced450cf1786e4b8f155180a49906ad
Filip Vacko <filip.vacko@gmail.com>
2025-03-21 16:17:10 +0100

    add flex to parent container

--- src/components/FrontPages/Hero.vue
+++ src/components/FrontPages/Hero.vue
@@ -1,18 +1,18 @@
 <template>
-  <div>
     <div
       v-if="route.meta.requiresAuth !== true"
-      class="bg-[url('../assets/images/bg.jpg')] h-screen bg-cover bg-black/30 bg-blend-overlay text-white"
+      class="bg-[url('../assets/images/bg.jpg')] h-screen bg-cover bg-black/30 bg-blend-overlay text-white flex items-start"
     >
       <Navigation />
-       <div
+       <!-- <div
+        id="header-subscription"
         v-if="route.name === '/'"
        >
         <slot name="header"></slot>
         <slot name="subscription"></slot>
-       </div>
+       </div> -->
+       <slot name="subscription"></slot>
     </div>
-  </div>
 </template>
 
 <script setup>
