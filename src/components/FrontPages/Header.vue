835b651364b7f70b769ab0706550e48ec5a92c2b
Filip Vacko <filip.vacko@gmail.com>
2025-03-23 16:45:15 +0100

    add CSS to header = finish header

--- src/components/FrontPages/Header.vue
+++ src/components/FrontPages/Header.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="pl-[100px]">{{ props.secondaryTitle }}</div>
-  <div class="pl-[100px]">{{ props.mainTitle }}</div>
+  <div class="leading-none">
+    <div class="pl-[100px] font-semibold text-[1.125rem] mb-12">{{ props.secondaryTitle }}</div>
+    <div class="pl-[100px] font-bold text-[2.5rem]">{{ props.mainTitle }}</div>
+  </div>
 </template>
 
 <script setup>
