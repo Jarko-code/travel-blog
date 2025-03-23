0dd8b228c2f303a3a7c87fb9954947d8c5921015
Filip Vacko <filip.vacko@gmail.com>
2025-03-23 16:50:27 +0100

    small header adjustment

--- src/components/FrontPages/Header.vue
+++ src/components/FrontPages/Header.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="leading-none">
-    <div class="pl-[100px] font-semibold text-[1.125rem] mb-12">{{ props.secondaryTitle }}</div>
-    <div class="pl-[100px] font-bold text-[2.5rem]">{{ props.mainTitle }}</div>
+  <div class="leading-none pl-[100px]">
+    <div class="font-semibold text-[1.125rem] mb-12">{{ props.secondaryTitle }}</div>
+    <div class="font-bold text-[2.5rem]">{{ props.mainTitle }}</div>
   </div>
 </template>
 
