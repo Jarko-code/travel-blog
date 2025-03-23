cf31ac8b362447f3359f1624294b699e18f9fadc
Filip Vacko <filip.vacko@gmail.com>
2025-03-23 17:09:35 +0100

    add elements and text

--- src/components/FrontPages/Subscription.vue
+++ src/components/FrontPages/Subscription.vue
@@ -1,7 +1,25 @@
 <template>
-  <div class="pl-[100px]">Subscription</div>
+  <div class="pl-[100px]">
+    <p>Subscribe and get the latest information's about our activities.</p>
+
+    <form @submit.prevent="sendSubscribeForm">
+      <div id="text-inputs">
+        <input v-model="firstName" placeholder="First name">
+        <input v-model="email" placeholder="Email">
+      </div>
+      <div id="subscription-bottom">
+        <input type="checkbox" v-model="isChecked" />
+        <span>
+          I consent to my personal information being processed in accordance with The Broke Backpacker’s <span>Privacy Policy</span>
+        </span>
+        <Button label="Subscribe" />
+      </div>
+    </form>
+
+  </div>
+
 </template>
 
 <script setup></script>
-
+  import Button from 'primevue/button';
 <style lang="scss"></style>
